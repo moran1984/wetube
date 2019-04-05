52a15ef47c28f3841b72c85a6d689eb04f5dd080
lukeFather <ryan.yun1126@gmail.com>
2019-04-05 10:53:04 +0900

    Wetube

---
+++ init.js
@@ -0,0 +1,14 @@
+import "./db";
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
+
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () => 
+    console.log(`✅ Listening on: http://localhost:${PORT}. Enjoy the coding master Yun 👀 `);
+
+app.listen(PORT, handleListening);
---
+++ models/Video.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is requred"
+    },
+    title: {
+        type: String,
+        requred: "Title is requred"
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"        
+    }]
+});
+
+const model = mongoose.model("Video", VideoSchema);
+
+export default model;
